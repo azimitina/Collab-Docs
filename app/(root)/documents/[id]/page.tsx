776a58f0781a663a0f739be5b62dd73208696081
Tina Azimi <tazimiaraghi@scentregroup.com>
2024-07-20 16:56:47 +1000

    Add clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,20 @@
+import Header from "@/components/Header";
 import { Editor } from "@/components/editor/Editor";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
   return (
     <div>
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">Share</div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
       <Editor />
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,15 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export const metadat: Metadata = {
+export const metadata: Metadata = {
   title: "Collab Docs",
   description: "Your collaborative docs",
 };
@@ -18,12 +20,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
